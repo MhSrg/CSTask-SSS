d3e4d4f5fcd428ae4c7f192722766f9dab596422
MhSrg <sergio.mh.solis@gmail.com>
2023-07-23 23:07:18 -0400

    animated movement

--- Assets/04-Scripts/Movement.cs
+++ Assets/04-Scripts/Movement.cs
@@ -6,12 +6,17 @@ public class Movement : MonoBehaviour
 {
     public float movementS = 10f;
     public Rigidbody2D rb;
+    public Animator animator;
     Vector2 movement;
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
