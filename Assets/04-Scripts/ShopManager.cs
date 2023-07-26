11aaf93e0a67af86e5c5ed9cc9dd75dd4977d29d
MhSrg <sergio.mh.solis@gmail.com>
2023-07-26 09:31:02 -0400

    shop 2

--- Assets/04-Scripts/ShopManager.cs
+++ Assets/04-Scripts/ShopManager.cs
@@ -9,7 +9,9 @@ public class ShopManager : MonoBehaviour
     public int gold;
     public TMP_Text goldUI;
     public SO_Items[] shopItems;
+    public GameObject[] shopPanelsGO;
     public ShopTemplate[] shopPanels;
+    public Button[] myPurchaseButtons;
 
     public Dialogue dialogueScript;
 
@@ -18,8 +20,13 @@ public class ShopManager : MonoBehaviour
 
     void Start()
     {
+        for (int i = 0; i < shopItems.Length; i++)
+        {
+            shopPanelsGO[i].SetActive(true);
+        }
         goldUI.text = gold.ToString();
         LoadItems();
+        CheckPurchaseable();
 
         chooseShop.SetActive(false);
         buyShop.SetActive(false);
@@ -35,9 +42,20 @@ public class ShopManager : MonoBehaviour
         buyShop.SetActive(true);
     }
 
+    public void PurchaseItem(int btnNo)
+    {
+        if (gold >= shopItems[btnNo].value)
+        {
+            gold = gold - shopItems[btnNo].value;
+            goldUI.text = gold.ToString();
+            CheckPurchaseable();
+        }
+    }
+
     public void AddGold()
     {
         goldUI.text = gold.ToString();
+        CheckPurchaseable();
     }
 
     public void LoadItems()
@@ -46,6 +64,21 @@ public class ShopManager : MonoBehaviour
         {
             shopPanels[i].titleTxt.text = shopItems[i].itemName;
             shopPanels[i].priceTxt.text = shopItems[i].value.ToString();
+        }
+    }
+
+    public void CheckPurchaseable()
+    {
+        for (int i = 0; i < shopItems.Length; i++)
+        {
+            if (gold >= shopItems[i].value)
+            {
+                myPurchaseButtons[i].interactable = true;
+            }
+            else
+            {
+                myPurchaseButtons[i].interactable = false;
+            }
         }
     }
 }
